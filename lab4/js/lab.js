31bbda9289ff73c280190955d25bb24eaf3e4094
omason19 <129993922+omason19@users.noreply.github.com>
2023-04-24 13:50:11 -0700

    lab 4
    lab 4 complete

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,13 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Pseudo code for Rock Paper Scissors
+// Author: Oliver Mason
+// Date:4/24/2023
 
-// Constants
+//Print "Select Rock Paper or Scissors (R, P, S)"
+  //Save input to variable
+    //Validate input to see if (Rock, rock, r, R, Paper, paper, P, p, Scissors, scissors, S, s)
+    //if not restart
+//Computer makes a random choice of Rock Paper Scissors
+//Compare user and computer results
+//Print results
+//Prompt for a new game
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
