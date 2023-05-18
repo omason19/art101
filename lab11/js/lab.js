b3ef5558d7c70af766b1f0d08217f78fb9e10b7d
Oliver <omason@ucsc.edu>
2023-05-17 23:03:16 -0700

    11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// lab.js - lab 11 jquery
+// Author: Oliver Mason
+// Date: 5/18/23
 
-// Constants
+var button = 
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
