d177bcf579a878e10a46cce0cb321547199f2b2b
Oliver <omason@ucsc.edu>
2023-05-24 22:58:17 -0700

    lab13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -3,28 +3,29 @@
 // Date: 5/25/23
 
 
-for (var i= 1; i <= 100; i++){
-	if (i % 3 == 0 && i % 5 == 0){
-  document.writeln("FizzBuzz")
-  $("#output").append("<div>" + "FizzBuzz");
-  } else if (i % 3 ==0){
-  document.writeln("Fizz")
-  $("#output").append("<div>" + "Buzz");
-  } else if(i % 5 == 0){
-  document.writeln("Buzz")
-  $("#output").append("<div>" + "Fizz");
-  } else{
-  document.writeln(i)
-  $("#output").append("<div>" + i);
+// for (var i= 1; i <= 100; i++){
+// 	if (i % 3 == 0 && i % 5 == 0){
+//   document.writeln("FizzBuzz")
+//   $("#output").append("<div>" + "FizzBuzz");
+//   } else if (i % 3 ==0){
+//   document.writeln("Fizz")
+//   $("#output").append("<div>" + "Buzz");
+//   } else if(i % 5 == 0){
+//   document.writeln("Buzz")
+//   $("#output").append("<div>" + "Fizz");
+//   } else{
+//   document.writeln(i)
+//   $("#output").append("<div>" + i);
 
-  }
-}
+//   }
+// }
 
 
 
 // other
 var str;
-for (var i= 1; i <= 110; i++){
+function go(){
+for (var i= 1; i <= 200; i++){
 	str = "";
   if (i % 3 ==0){
   str += "Fizz"
@@ -41,4 +42,6 @@ for (var i= 1; i <= 110; i++){
   str = i}
   $("#output").append("<div>" + str);
 }
+}
 
+go()
